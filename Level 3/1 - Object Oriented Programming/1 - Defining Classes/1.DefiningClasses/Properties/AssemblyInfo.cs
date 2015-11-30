3ae5e662185c93c9aa19ec47ec919b5170c9de95
Bare7a <bare7a@gmail.com>
2015-11-30 02:00:16 +0200

    Software University

--- Level 3/1 - Object Oriented Programming/1 - Defining Classes/1.DefiningClasses/Properties/AssemblyInfo.cs
+++ Level 3/1 - Object Oriented Programming/1 - Defining Classes/1.DefiningClasses/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("1 - Defining Classes")]
+[assembly: AssemblyTitle("1.Defining Classes")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("1 - Defining Classes")]
+[assembly: AssemblyProduct("1.Defining Classes")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]

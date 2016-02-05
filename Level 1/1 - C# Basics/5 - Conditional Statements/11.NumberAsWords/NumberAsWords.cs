cd0cf681684ac4a690922954ba51ab18829fc3dd
Bare7a <bare7a@gmail.com>
2016-02-05 23:16:54 +0200

    Software University

--- Level 1/1 - C# Basics/5 - Conditional Statements/11.NumberAsWords/NumberAsWords.cs
+++ Level 1/1 - C# Basics/5 - Conditional Statements/11.NumberAsWords/NumberAsWords.cs
@@ -6,84 +6,86 @@ namespace NumberAsWords
     {
         static void Main(string[] args)
         {
-            Console.Write("Write a number between 0 and 999: ");
-            int num = int.Parse(Console.ReadLine());
-            if (num >= 0 && num <= 20)
+            Console.Write("Input a number between [0...999]: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number >= 0 && number <= 999)
             {
-                NumFrom0to19and10s(num);
-            }
-            else if (num >= 21 && num <= 999)
-            {
-                if (num / 100 == 0)
+                string result = String.Empty;
+
+                int hundreds = number / 100;
+                int decimals = (number / 10) % 10;
+                int digitals = number % 10;
+
+                if (hundreds > 0)
                 {
-                    NumFrom0to19and10s((num / 10) * 10);
-                    if (num % 10 != 0)
+                    result += NumbersAsWords(hundreds) + " Hundred";
+                    if (decimals != 0 || digitals != 0)
                     {
-                        NumFrom0to19and10s(num % 10);
+                        result += " and ";
+                    }
+                }
+
+                if (decimals == 0 || decimals == 1)
+                {
+                    if (!(hundreds != 0 && (decimals + digitals) == 0))
+                    {
+                        result += NumbersAsWords(decimals * 10 + digitals);
                     }
                 }
                 else
                 {
-                    NumFrom0to19and10s(num / 100);
-                    Console.Write("Hundred ");
-                    if (num % 100 > 19)
+                    result += NumbersAsWords(decimals * 10);
+                    if (decimals != 0)
                     {
-                        Console.Write("and ");
-                        NumFrom0to19and10s((num % 100 / 10) * 10);
-                        if (num % 10 != 0)
-                        {
-                            NumFrom0to19and10s(num % 10);
-                        }
-                    }
-                    else if(num%100 !=0 && num%100<20)
-                    {
-                        Console.Write("and ");
-                        NumFrom0to19and10s(num % 100);
+                        result += "-" + NumbersAsWords(digitals);
                     }
                 }
+
+                Console.WriteLine(result);
             }
             else
             {
-                Console.WriteLine("Incorrect input!");
+                Console.WriteLine("Incorrect Input!");
             }
-            Console.WriteLine();
+
+
         }
 
-        static public void NumFrom0to19and10s(int x)
+        private static string NumbersAsWords(int number)
         {
-            switch (x)
+            switch (number)
             {
-                case 0: Console.Write("Zero "); break;
-                case 1: Console.Write("One "); break;
-                case 2: Console.Write("Two "); break;
-                case 3: Console.Write("Three "); break;
-                case 4: Console.Write("Four "); break;
-                case 5: Console.Write("Five "); break;
-                case 6: Console.Write("Six "); break;
-                case 7: Console.Write("Seven "); break;
-                case 8: Console.Write("Eight "); break;
-                case 9: Console.Write("Nine "); break;
-                case 10: Console.Write("Ten "); break;
-                case 11: Console.Write("Eleven "); break;
-                case 12: Console.Write("Twelve "); break;
-                case 13: Console.Write("Thirteen "); break;
-                case 14: Console.Write("Fourteen "); break;
-                case 15: Console.Write("Fifteen "); break;
-                case 16: Console.Write("Sixteen "); break;
-                case 17: Console.Write("Seventeen "); break;
-                case 18: Console.Write("Eighteen "); break;
-                case 19: Console.Write("Nineteen "); break;
-                case 20: Console.Write("Twenty "); break;
-                case 30: Console.Write("Thirty "); break;
-                case 40: Console.Write("Forty "); break;
-                case 50: Console.Write("Fifty "); break;
-                case 60: Console.Write("Sixty "); break;
-                case 70: Console.Write("Seventy "); break;
-                case 80: Console.Write("Eighty "); break;
-                case 90: Console.Write("Ninety "); break;
-                default: Console.Write(""); break;
+                case 0: return "Zero"; break;
+                case 1: return "One"; break;
+                case 2: return "Two"; break;
+                case 3: return "Three"; break;
+                case 4: return "Four"; break;
+                case 5: return "Five"; break;
+                case 6: return "Six"; break;
+                case 7: return "Seven"; break;
+                case 8: return "Eight"; break;
+                case 9: return "Nine"; break;
+                case 10: return "Ten"; break;
+                case 11: return "Eleven"; break;
+                case 12: return "Twelve"; break;
+                case 13: return "Thirteen"; break;
+                case 14: return "Fourteen"; break;
+                case 15: return "Fifteen"; break;
+                case 16: return "Sixteen"; break;
+                case 17: return "Seventeen"; break;
+                case 18: return "Eighteen"; break;
+                case 19: return "Nineteen"; break;
+                case 20: return "Twenty"; break;
+                case 30: return "Thirty"; break;
+                case 40: return "Forty"; break;
+                case 50: return "Fifty"; break;
+                case 60: return "Sixty"; break;
+                case 70: return "Seventy"; break;
+                case 80: return "Eighty"; break;
+                case 90: return "Ninety"; break;
+                default: return ""; break;
             }
         }
-
     }
 }
